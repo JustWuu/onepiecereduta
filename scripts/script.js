9e02d6c6c70e642882418e15a0b3c4f57fb51c19
rty21785 <rty21785@gmail.com>
2022-09-26 23:16:11 +0800

    完成影片區大致內容

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$("#mv-1").click(function(){
+    $(".ga-1").css("display", "list-item");
+    $(".diary").css("display", "none");
+    
+}); 
+$("#diary-1").click(function(){
+    $(".diary").css("display", "list-item");
+    $(".ga-1").css("display", "none");
+}); 
