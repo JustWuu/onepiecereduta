68b1675f52111e79090aaa75569e0a83a50f2619
rty21785 <rty21785@gmail.com>
2022-09-30 22:45:33 +0800

    JS改寫、購物車完成、商品介紹完成，結算製作中

--- scripts/script.js
+++ scripts/script.js
@@ -23,7 +23,7 @@ function comStatistics(buycar,nbuy){
             allmoney += nbuy.money
 
             $(`.${nbuy.comID}`).html(`<div class="carCenter"><p>${nbuy.comName}</p></div><div class="carMoney "><p>${buycar[i].comQua}個</p><p>${buycar[i].money}$</p></div><div class="carCenter"><button id="remove${nbuy.comID}" onclick='removeComm("${nbuy.comID}")'>移除</button></div>`)
-            $(".total").html(`<p>總共金額：${allmoney} $</p><button>結算</button>`)
+            $(".total").html(`<p>總共金額：${allmoney} $</p><button onclick="clicktotal()">結算</button>`)
 
             return console.log("更新");
         }
@@ -31,7 +31,7 @@ function comStatistics(buycar,nbuy){
     car.push(nbuy);
     $(".ul-buy").append(`<li class="li-buy ${nbuy.comID}"><div class="carCenter"><p>${nbuy.comName}</p></div><div class="carMoney "><p>${nbuy.comQua}個</p><p>${nbuy.money}$</p></div><div class="carCenter"><button id="remove${nbuy.comID}" onclick='removeComm("${nbuy.comID}")')>移除</button></div></li>`)
     allmoney += nbuy.money
-    $(".total").html(`<p>總共金額：${allmoney} $</p><button>結算</button>`)
+    $(".total").html(`<p>總共金額：${allmoney} $</p><button onclick="clicktotal()">結算</button>`)
     return console.log("新增")
 }
 
@@ -41,7 +41,7 @@ function removeComm(comID){
             allmoney -= car[i].money
             car.splice(i,1)
             $(`.${comID}`).remove()
-            $(".total").html(`<p>總共金額：${allmoney} $</p><button>結算</button>`)
+            $(".total").html(`<p>總共金額：${allmoney} $</p><button onclick="clicktotal()">結算</button>`)
             console.log(car)
         }
     }
@@ -50,31 +50,46 @@ function removeComm(comID){
 // 通用函式結束
 
 
+// 全商品共用JS
+function buyclick(ID){
+    comStatistics(car,{comName:$(`.${ID}-name`).text(),comID:ID,comQua:Number($(`.com${ID}`).val()),money:Number($(`.com${ID}`).val())*Number($(`.${ID}-money`).text())})
+    console.log(car)
+    window.alert("已加入購物車")
+}
+// 
+
+
 // 每個商品
-$("#comA0001").click(function(){
-    comStatistics(car,{comName:"衣服",comID:"A0001",comQua:Number($(".comA0001").val()),money:Number($(".comA0001").val())*200})
-    console.log(car)
-    window.alert("已加入購物車")
-}); 
+// $("#comA0001").click(function(){
+//     comStatistics(car,{comName:"衣服",comID:"A0001",comQua:Number($(".comA0001").val()),money:Number($(".comA0001").val())*200})
+//     console.log(car)
+//     window.alert("已加入購物車")
+// }); 
 
-$("#comA0002").click(function(){
-    comStatistics(car,{comName:"褲子",comID:"A0002",comQua:Number($(".comA0002").val()),money:Number($(".comA0002").val())*300})
-    console.log(car)
-    window.alert("已加入購物車")
-}); 
-
-$("#comA0003").click(function(){
-    comStatistics(car,{comName:"帽子",comID:"A0003",comQua:Number($(".comA0003").val()),money:Number($(".comA0003").val())*100})
-    console.log(car)
-    window.alert("已加入購物車")
-}); 
-
-$("#comA0004").click(function(){
-    comStatistics(car,{comName:"鞋子",comID:"A0004",comQua:Number($(".comA0004").val()),money:Number($(".comA0004").val())*500})
-    console.log(car)
-    window.alert("已加入購物車")
-}); 
-
-
-
+// $("#comA0002").click(function(){
+//     comStatistics(car,{comName:"褲子",comID:"A0002",comQua:Number($(".comA0002").val()),money:Number($(".comA0002").val())*300})
+//     console.log(car)
+//     window.alert("已加入購物車")
+// }); 
 // 每個商品結束
+
+
+// 展示
+function closeExh(){
+    $(".topexhibit").css("display", "none");
+    $("#info").css("display", "none");
+    $("#bill").css("display", "none");
+}
+
+function clickImg(ID){
+    $("#info").html(`<div class="info-img"><img src="images/99023925_p1.png" alt=""></div>
+    <div class="info-p"><p>${$(`.${ID}-name`).text()}</p><p>${$(`.${ID}-info`).text()}</p><p>${$(`.${ID}-money`).text()}$</p></div>`)
+    $(".topexhibit").css("display", "flex");
+    $("#info").css("display", "flex");
+}
+
+function clicktotal(){
+    $(".topexhibit").css("display", "flex");
+    $("#bill").css("display", "flex");
+}
+// 
